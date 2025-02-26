0ddc8848c765ce3c8872fcfceccaecaba9327f56
Antonio Felipe Souza <antonio.souza@sptech.school>
2025-02-26 18:48:19 -0300

    Iniciando sessao 1

---
+++ app-wego/src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+function Home() {
+ 
+  return (
+    <main>
+      <header className='header'>
+        <div className='logo'>
+          <div>we</div>
+          <div>go</div>
+        </div>
+        <ul>
+          <li>Sobre nós</li>
+          <li>Equipe</li>
+          <li>Projeto</li>
+          <li>PeTinder</li>
+        </ul>
+      </header>
+
+    </main>
+      
+  )
+}
+
+export default Home
