78d3e05bd6179a9a3ae2c042d5b28387f7248878
FelipeSouza <antonio.souza@sptech.school>
2025-03-16 18:44:14 -0300

    site institucional finalizado sprint 1

--- app-wego/src/Home/pages/index.jsx
+++ app-wego/src/Home/pages/index.jsx
@@ -1,38 +1,37 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-scroll';
 import waveImg from "../../assets/wave2.svg";
 import waveReverseImg from "../../assets/wavereverse2.svg";
 import linkExtImg from "../../assets/linkExt.svg";
-import testeImg from "../../assets/halftone-backg2.svg";
+import snake from "../../assets/finalWave2.svg";
 import { Swiper } from 'swiper/react';
-import { GlobalStyle, Navbar, Logo, Ul, Anchor, SectionOne, Title, H1, H2, Baloons, Baloon, SectionTwo, ContainerLeft, Left, H11, P, Right, ContainerRight, LogoTitle, LogoTitleH1, SectionThree, H12, Card, Wave, SectionFour, Container, LeftContainer, H13, P1, Button, LinkExt, ProjectImages, SectionFive, WaveReverse, Shortcuts, UlShortcuts, Copyright, Map, ImgCarrossel, H14 } from "./style";
-
-import { StyledSwiperSlide, Name, Function, Desc, Teste, Arrow } from './style';
+import { GlobalStyle, Navbar, Logo, Ul, SectionOne, Title, H1, H2, Baloons, Baloon, SectionTwo, ContainerLeft, Left, H11, P, Right, ContainerRight, LogoTitle, LogoTitleH1, SectionThree, H12, Card, Wave, SectionFour, Container, LeftContainer, H13, P1, Button, LinkExt, ProjectImages, SectionFive, WaveReverse, Shortcuts, UlShortcuts, Copyright, Map, ImgCarrossel, H14, StyledSwiperSlide, Name, Function, Desc, SnakeBG , Arrow } from "./style";
 
 const Home = () => {
     const data = [
         {
             id: 1, image: 'https://avatars.githubusercontent.com/u/160804980?v=4', nome: 'Antonio Felipe', cargo: 'Front-End',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            desc: 'Com habilidades diversificadas em tecnologia, atualmente o foco está no desenvolvimento e aplicação de soluções no front-end. Onde o objetivo é criar interfaces modernas,  interativas e centradas na experiência do usuário, utilizando práticas de UI/UX como prototipação e ajustes contínuos no design para garantir a melhor usabilidade. Ao combinar essas abordagens, os produtos não só atendem às necessidades específicas dos usuários, mas também proporcionam uma navegação fluida, intuitiva e envolvente, visando otimizar a interação e a satisfação do usuário em todos os pontos de contato.'
         },
         {
-            id: 2, image: 'https://avatars.githubusercontent.com/u/122701467?v=4', nome: 'Guilherme Sousa', cargo: 'Back-End',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            id: 2, image: 'https://avatars.githubusercontent.com/u/160186899?v=4', nome: 'Cauan Araruna', cargo: 'DBA',
+            desc: 'Envolvido no planejamento estratégico do gerenciamento de dados, assegurando eficiência e organização no armazenamento. Inclui também a coleta de informações para garantir que os dados coletados estejam condizentes com a realidade do cliente, devidamente estruturados e armazenados com segurança. Além disso, realiza o desenvolvimento de wireframes e visualizações, ajustando o fluxo do sistema e a experiência do usuário, garantindo que as soluções entregues estejam sempre em sintonia com os dados processados e as necessidades do projeto.'
         },
         {
-            id: 3, image: 'https://avatars.githubusercontent.com/u/177654634?v=4', nome: 'Igor Chu', cargo: 'Tech-Lead',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            id: 3, image: 'https://avatars.githubusercontent.com/u/122701467?v=4', nome: 'Guilherme Sousa', cargo: 'Back-End',
+            desc: 'Com experiência sólida no back-end, a atuação envolve a criação de soluções eficientes e escaláveis, garantindo a integração entre diferentes sistemas e camadas da aplicação. O foco está na construção de endpoints que facilitam o gerenciamento de dados essenciais, como informações de usuários e arquivos, além de otimizar a forma como esses dados são armazenados e acessados. A utilização de boas práticas de comunicação entre os serviços assegura que o sistema seja ágil, seguro e responsivo, atendendo às necessidades dos usuários e equipe de forma eficaz.'
         },
         {
-            id: 4, image: 'https://avatars.githubusercontent.com/u/160802109?v=4', nome: 'Laise Martin', cargo: 'Front-End',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            id: 4, image: 'https://avatars.githubusercontent.com/u/177654634?v=4', nome: 'Igor Chu', cargo: 'Tech-Lead',
+            desc: 'Um desenvolvedor full stack e líder tecnológico, conhecido por sua abordagem positiva e proativa. Com experiência em diversas tecnologias, ele se destaca na criação de soluções inovadoras e na liderança de times, sempre incentivando a colaboração e o crescimento profissional. Seu foco está em desenvolver produtos de alto impacto, garantindo qualidade, performance e usabilidade. Além disso, ele tem paixão por tecnologia e está sempre em busca de novos desafios para impulsionar sua carreira e contribuir para o sucesso das equipes com as quais trabalha.'
         },
         {
-            id: 5, image: 'https://avatars.githubusercontent.com/u/161353790?v=4', nome: 'Naomi Yuumi', cargo: 'Project Oner',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            id: 5, image: 'https://avatars.githubusercontent.com/u/160802109?v=4', nome: 'Laise Martin', cargo: 'Front-End',
+            desc: 'Focada no desenvolvimento front-end, está constantemente aprendendo novas abordagens e técnicas para aprimorar a criação de interfaces. Seus feitos envolvem a construção de componentes essenciais para a interação do usuário, sempre priorizando a usabilidade e a estética. Além de se envolver ativamente em outras etapas do processo de negócios, como documentação e elicitação de requisitos, assegurando que todas as soluções estejam alinhadas às necessidades e expectativas do projeto.'
         },
         {
-            id: 6, image: 'https://rlv.zcache.com.br/poster_rosto_scooby_doo_sorrindo-r27421e2266664c659a0f12d8939fd7d9_geub_8byvr_644.webp', nome: 'Cauan Araruna', cargo: 'DevOps',
-            desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!'
+            id: 6, image: 'https://avatars.githubusercontent.com/u/161353790?v=4', nome: 'Naomi Calisto', cargo: 'Project Owner',
+            desc: 'Atuando na gestão estratégica das demandas do projeto, com foco na entrega dos requisitos para garantir que as soluções atendam às reais necessidades dos clientes. O contato direto com o cliente final permite entender suas expectativas e traduzir isso em requisitos claros e objetivos. Além disso, o gerenciamento dos processos é feito de forma eficiente, assegurando que as etapas do projeto sejam devidamente cumpridas e com alta qualidade. O relacionamento saudável com a equipe é sempre priorizado, promovendo uma comunicação aberta e colaborativa, essencial para o sucesso do projeto.'
         }
     ]
 
@@ -66,16 +65,16 @@ const Home = () => {
     return (
         <>
             <GlobalStyle />
-            <Navbar>
+            <Navbar id='inicio'>
                 <Logo>
                     <div>we</div>
                     <div>go</div>
                 </Logo>
                 <Ul>
-                    <li><Anchor href="#sobre-nos">Sobre nós</Anchor></li>
-                    <li><Anchor href="#equipe">Equipe</Anchor></li>
-                    <li><Anchor href="#petinder">Projeto</Anchor></li>
-                    <li><Anchor href="#petinder">PeTinder</Anchor></li>
+                    <li><Link to='sobre-nos' smooth={true} duration={700}>Sobre nós</Link></li>
+                    <li><Link to='equipe' smooth={true} duration={700}>Equipe</Link></li>
+                    <li><Link to='petinder' smooth={true} duration={700}>Projeto</Link></li>
+                    <li><Link to='#' smooth={true} duration={700}>PeTinder</Link></li>
                 </Ul>
             </Navbar>
 
@@ -86,7 +85,7 @@ const Home = () => {
                     <H2>em direção ao futuro.</H2>
                 </Title>
 
-                <Teste src={testeImg}></Teste>
+                <SnakeBG src={snake}></SnakeBG>
 
                 <Baloons>
                     <Baloon>Inovação</Baloon>
@@ -94,7 +93,7 @@ const Home = () => {
                     <Baloon>Inteligência</Baloon>
                 </Baloons>
 
-                <Anchor href="#sobre-nos"><Arrow></Arrow></Anchor>
+                <Link to='sobre-nos' smooth={true} duration={700}><Arrow></Arrow></Link>
             </SectionOne>
 
             <SectionTwo id='sobre-nos'>
@@ -102,11 +101,13 @@ const Home = () => {
                     <Left>
                         <H11>WeGo Solutions</H11>
                         <P>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam suscipit tempora recusandae cumque, at quo sed. Quod corrupti, iure ipsum nesciunt quia obcaecati rerum non voluptates doloribus ullam, harum a?
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis distinctio dolor deleniti ea nisi aut repellat non rem cupiditate assumenda? A eveniet, quidem excepturi cumque error laudantium alias exercitationem quia.
-
+                        Criada com a missão de desenvolver soluções tecnológicas inovadoras que impactam positivamente a sociedade. Combinamos tecnologia e propósito para criar aplicações intuitivas e eficientes, sempre focadas na experiência do usuário e na resolução de desafios reais.
+                        </P>
+                        <P>
+                        Fundada por estudantes da SPTech, a WeGo surgiu da união de talentos que,atuando como estagiários em diferentes empresas, compartilharam a visão de transformar o mundo por meio da tecnologia. Aplicamos metodologias ágeis e inovação contínua para desenvolver soluções escaláveis, conectando pessoas e otimizando processos.
+                        </P> 
+                        <P>
+                        Acreditamos que a tecnologia tem o poder de gerar impacto e promover um futuro mais inteligente e acessível. Nosso compromisso é entregar projetos de qualidade, impulsionando mudanças significativas em diversos setores.
                         </P>
                     </Left>
 
@@ -140,14 +141,13 @@ const Home = () => {
             </SectionThree>
 
             <SectionFour id='petinder'>
-                <H14>Nossos projetos</H14>
+                <H14>Projeto Principal</H14>
                 <Container>
                     <LeftContainer>
                         <H13>Conheça o PeTinder</H13>
                         <P1>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime maiores quam, voluptates quibusdam facere aut quisquam      dignissimos possimus, neque ipsum, eaque ea tempora reiciendis autem accusantium voluptatem placeat adipisci.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptatum sunt excepturi sequi atque dignissimos odit reiciendis neque eos iure! Ab cumque, assumenda illo alias qui sint voluptatem nemo quos!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam suscipit tempora recusandae cumque, at quo sed.
+                            Uma aplicação que combina tecnologia e propósito social, visando facilitar o processo de adoção de animais. Inspirado no conceito do Tinder, a plataforma permite que os adotantes deem "match" com pets cadastrados pela ONG AUmigos do Bem, parceira inicial do projeto. 
+                            <br />Ao explorar os perfis dos animais, os usuários podem escolher aqueles que mais se identificam, simplificando a conexão entre pets e possíveis adotantes. A ferramenta busca promover a adoção responsável de forma prática e acessível, apoiando o trabalho da ONG e ajudando mais animais a encontrarem um lar amoroso.
                         </P1>
 
                         <Button>
@@ -164,11 +164,11 @@ const Home = () => {
                 <WaveReverse src={waveReverseImg}></WaveReverse>
                 <Shortcuts>
                     <UlShortcuts>
-                        <li><Anchor href="">WeGo</Anchor></li>
-                        <li><Anchor href="#sobre-nos">Sobre nós</Anchor></li>
-                        <li><Anchor href="#equipe">Equipe</Anchor></li>
-                        <li><Anchor href="#projeto">Projeto</Anchor></li>
-                        <li><Anchor href="#petinder">PeTinder</Anchor></li>
+                    <li><Link to='inicio' smooth={true} duration={700}>WeGo</Link></li>
+                    <li><Link to='sobre-nos' smooth={true} duration={700}>Sobre nós</Link></li>
+                    <li><Link to='equipe' smooth={true} duration={700}>Equipe</Link></li>
+                    <li><Link to='petinder' smooth={true} duration={700}>Projeto</Link></li>
+                    <li><Link to='#' smooth={true} duration={700}>PeTinder</Link></li>
                     </UlShortcuts>
 
                     <Copyright>© WeGo Solutions</Copyright>
@@ -178,7 +178,7 @@ const Home = () => {
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.2874821816254!2d-46.66418892378846!3d-23.55811636147456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59d2b063f663%3A0xc567c0d16d0bc582!2sS%C3%A3o%20Paulo%20Tech%20School!5e0!3m2!1spt-BR!2sbr!4v1741144649786!5m2!1spt-BR!2sbr"
                         style={{
-                            border: '7px solid #D4D9EE',
+                            border: '5px solid #D4D9EE',
                             borderRadius: '10px',
                             width: '80%',
                             height: '60%',
